ea985e3fcb763ce58727dad450699679818cb80d
Michael-McCray <macoovae@gmail.com>
2026-01-07 20:55:51 -0500

    DP-597 <message>

---
+++ app/michael-mccray/page.tsx
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: 'Michael McCray',
+}
+
+export default function MichaelMcCrayPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-[#205495]">
+      
+
+      <img src="/card2.png" alt="Michael McCray" width={800} height={600} />
+    </div>
+  )
+}
+
