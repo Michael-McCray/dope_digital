2240a7e13f02c1df242edb67b164568d152734e5
Michael-McCray <macoovae@gmail.com>
2026-01-11 22:50:29 -0500

    card

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -46,7 +46,9 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex-shrink-0">
-            <img src="/logo.png" alt="Logo" className="h-[auto] w-[150px]" />
+            <a href="#home">
+              <img src="/logo.png" alt="Logo" className="h-[auto] w-[150px]" />
+            </a>
           </div>
           <div className="hidden md:block">
             <div className="ml-10 flex items-baseline space-x-8">
